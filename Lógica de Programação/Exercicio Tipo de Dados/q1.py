e358a5ea2d5b30ccd2698b5eb6e05770ce41e265
Carolaine Silva <90916225+Carol316@users.noreply.github.com>
2024-06-11 02:21:43 -0300

    Delete Lógica de Programação/Exercicio Tipo de Dados directory

--- Lógica de Programação/Exercicio Tipo de Dados/q1.py
+++
@@ -1,9 +0,0 @@
-
-def raiz_cubica(numero):
-  
-  return numero ** (1/3)
-
-numero = float(input("Digite um número: "))
-raiz_cubica = raiz_cubica(numero)
-
-print(f"A raiz cúbica de {numero} é {raiz_cubica}")
--- Lógica de Programação/Exercicio Tipo de Dados/q3.py
+++
@@ -1,7 +0,0 @@
-hora = int(input("Digite a hora: "))
-minutos = int(input("Digite os minutos: "))
-segundos = int(input("Digite os segundos: "))
-
-total_segundos = hora * 3600 + minutos * 60 + segundos
-
-print(f"Total de segundos desde a meia-noite: {total_segundos} segundos.")
